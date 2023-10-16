e16e8d5014e60b23c204c150addf1eb785fbfd35
William Chen <williamchen@WilliamdeMacBook-Air.local>
2023-10-17 00:39:16 +0800

    initial minimal-mistakes-master

---
+++ _posts/2023-10-17-test-article.md
@@ -0,0 +1,13 @@
+---
+title: "This is a testing article."
+date: 2023-10-17 00:00:00 +0800
+
+tags:
+- blog-test
+---
+
+# This is a testing article.
+#### tag: `blog-test`
+
+## h2title
+### h3title
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "This is a testing article.",
+        "excerpt":"This is a testing article.  tag: blog-test   h2title  h3title  ","categories": [],
+        "tags": ["blog-test"],
+        "url": "/test-article/",
+        "teaser": null
+      }]
